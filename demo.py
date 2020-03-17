40a1dab458cd2567d552c95a8a0d88b63d3241dc
Telmo Pires <tjppires@gmail.com>
2020-03-17 11:23:59 -0400

    Updated demo.

--- demo.py
+++ demo.py
@@ -1,40 +1,43 @@
+import cv2
+import matplotlib.pyplot as plt
 import numpy as np
-from scipy.misc import imread
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
-from FlowNet2_src import FlowNet2, LONG_SCHEDULE
-from FlowNet2_src import flow_to_image
+import flowlib
+import models
 
 
-if __name__ == '__main__':
-  # Graph construction
-  im1_pl = tf.placeholder(tf.float32, [1, 384, 512, 3])
-  im2_pl = tf.placeholder(tf.float32, [1, 384, 512, 3])
+def load_image(path: str) -> np.ndarray:
+  img = cv2.imread(path, cv2.IMREAD_COLOR)
 
-  flownet2 = FlowNet2()
-  inputs = {'input_a': im1_pl, 'input_b': im2_pl}
-  flow_dict = flownet2.model(inputs, LONG_SCHEDULE, trainable=False)
-  pred_flow = flow_dict['flow']
+  # OpenCV stores images as BGR, but the model expects RGB.
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-  # Feed forward
-  im1 = imread('FlowNet2_src/example/0img0.ppm')/255.
-  im2 = imread('FlowNet2_src/example/0img1.ppm')/255.
-  im1 = np.array([im1]).astype(np.float32)
-  im2 = np.array([im2]).astype(np.float32)
+  # Convert to float and add batch dimension.
+  return np.expand_dims(img.astype(np.float32) / 255., axis=0)
 
-  ckpt_file = 'FlowNet2_src/checkpoints/FlowNet2/flownet-2.ckpt-0'
+
+if __name__ == "__main__":
+  # Graph construction.
+  image1_placeholder = tf.placeholder(tf.float32, [1, 384, 512, 3])
+  image2_placeholder = tf.placeholder(tf.float32, [1, 384, 512, 3])
+  flownet2 = models.FlowNet2()
+  inputs = {"input_a": image1_placeholder, "input_b": image2_placeholder}
+  predicted_flow = flownet2.run(inputs)["flow"]
+
+  # Load inputs.
+  image1 = load_image("example/0img0.ppm")
+  image2 = load_image("example/0img1.ppm")
+
+  ckpt_file = "checkpoints/FlowNet2/flownet-2.ckpt-0"
   saver = tf.train.Saver()
 
   with tf.Session() as sess:
     saver.restore(sess, ckpt_file)
-    # Double check loading is correct
-    #for var in tf.all_variables():
-    #  print(var.name, var.eval(session=sess).mean())
-    feed_dict = {im1_pl: im1, im2_pl: im2}
-    pred_flow_val = sess.run(pred_flow, feed_dict=feed_dict)
+    feed_dict = {image1_placeholder: image1, image2_placeholder: image2}
+    predicted_flow_np = sess.run(predicted_flow, feed_dict=feed_dict)
 
-  # Visualization
-  import matplotlib.pyplot as plt
-  flow_im = flow_to_image(pred_flow_val[0])
-  plt.imshow(flow_im)
+  # Show visualization.
+  flow_image = flowlib.flow_to_image(predicted_flow_np[0])
+  plt.imshow(flow_image)
   plt.show()
